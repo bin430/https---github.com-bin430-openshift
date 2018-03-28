267a98aa18034cebe00dedb7f78003524767e7ef
bin430 <444199393@qq.com>
2018-03-28 10:33:20 +0800

    V0.0.6

--- weixin.php
+++ weixin.php
@@ -1,8 +1,75 @@
 <?php
-if (isset($url))
+/**
+ * wechat php test
+ */
+
+//define your token
+define("TOKEN", "weixin");
+$wechatObj = new wechatCallbackapiTest();
+$wechatObj->valid();//总入口
+
+class wechatCallbackapiTest
 {
-Header("HTTP/1.1 303 See Other");
-Header("Location: $url");
-exit;
+    public function valid()
+    {
+        $echoStr = $_GET["echostr"];
+        //valid signature , option
+        if (strlen($echoStr)>0) {
+            //接口验证
+            $this->checkSignature();
+            echo $echoStr;
+        }else{
+            $postStr = file_get_contents('php://input', 'r');
+            send_post("http://218.66.48.231:30159/index.php",$postStr);
+        }
+    }
+
+    private function checkSignature()
+    {
+        // you must define TOKEN by yourself
+        if (!defined("TOKEN")) {
+            throw new Exception('TOKEN is not defined!');
+        }
+
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        // use SORT_STRING rule
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * 发送post请求
+     * @param string $url 请求地址
+     * @param array $post_data post键值对数据
+     * @return string
+     */
+    function send_post($url, $post_data) {
+     
+      $postdata = http_build_query($post_data);
+      $options = array(
+        'http' => array(
+          'method' => 'POST',
+          'header' => 'Content-type:application/x-www-form-urlencoded',
+          'content' => $postdata,
+          'timeout' => 15 * 60 // 超时时间（单位:s）
+        )
+      );
+      $context = stream_context_create($options);
+      $result = file_get_contents($url, false, $context);
+     
+      return $result;
+}
 }
 ?>
